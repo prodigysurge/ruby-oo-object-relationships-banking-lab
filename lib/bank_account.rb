dd4cf8443787c6905ff8e536b1058134efcd3f62
prodigysurge <boatinglife6@yahoo.com>
2021-01-26 14:54:54 -0500

    commit tree 8d4734c81ae19c51e1c149aab496186f91ee650f

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,43 @@
 class BankAccount
 
+    attr_accessor :status, :balance
+    attr_reader :name
+
+    # @@balance = []
+
+    def initialize(name, balance = 1000, status = "open")
+        @name = name
+        @balance = balance
+        @status = status
+        
+    end
+
+    def name
+        @name
+    end
+
+    def balance
+        @balance
+    end
+
+    def status
+        @status
+    end
+
+    def deposit(deposit)
+        self.balance += deposit
+    end
+
+    def display_balance
+        "Your balance is $#{self.balance}."
+    end
+
+    def valid?
+        status == "open" && balance > 0
+    end
+
+    def close_account
+        self.status = "closed"
+    end
+
 end
